ff6cdedb45f5a8d87903dfce1d907902d48ba9f0
TheBestShooter <rnolascoc@uni.pe>
2021-01-24 23:03:42 -0500

    updated sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,16 +12,17 @@ VALUES (2, 'under armour Men ''s Micro G Assert – 7','under armour Men ''Light
 INSERT INTO tbl_products (id, name, description, stock,price,status, create_at,category_id)
 VALUES (3, 'Spring Boot in Action','under armour Men '' Craig Walls is a software developer at Pivotal and is the author of Spring in Action',12,40.06,'CREATED','2018-09-05',2);
 
-INSERT INTO tbl_regions (id, name) VALUES (1, 'Sudamérica');
-INSERT INTO tbl_regions (id, name) VALUES (2, 'Centroamérica');
-INSERT INTO tbl_regions (id, name) VALUES (3, 'Norteamérica');
+INSERT INTO tbl_regions (id, name) VALUES (1, 'Sudamerica');
+INSERT INTO tbl_regions (id, name) VALUES (2, 'Centroamerica');
+INSERT INTO tbl_regions (id, name) VALUES (3, 'Norteamerica');
 INSERT INTO tbl_regions (id, name) VALUES (4, 'Europa');
 INSERT INTO tbl_regions (id, name) VALUES (5, 'Asia');
 INSERT INTO tbl_regions (id, name) VALUES (6, 'Africa');
-INSERT INTO tbl_regions (id, name) VALUES (7, 'Oceanía');
-INSERT INTO tbl_regions (id, name) VALUES (8, 'Antártida');
+INSERT INTO tbl_regions (id, name) VALUES (7, 'Oceania');
+INSERT INTO tbl_regions (id, name) VALUES (8, 'Antartida');
 
-INSERT INTO tbl_customers (id,number_id,first_name,last_name , email, photo_url, region_id, state) VALUES(1,'32404580', 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com','',1,'CREATED');
+INSERT INTO tbl_customers (id,number_id,first_name,last_name , email, photo_url, region_id, state) VALUES(1,'76154725', 'Andres', 'Guzman', 'aguzman@gmail.com','',1,'CREATED');
+INSERT INTO tbl_customers (id,number_id,first_name,last_name , email, photo_url, region_id, state) VALUES(2,'76146602', 'Ronaldo', 'Nolasco', 'rnolascochavez@gmail.com','',1,'CREATED');
 
 INSERT INTO tlb_invoices (id, number_invoice, description, customer_id, create_at, state) VALUES(1, '0001', 'invoice office items', 1, NOW(),'CREATED');
 
